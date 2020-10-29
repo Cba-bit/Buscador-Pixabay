bef383c68c4490b464f1124ef45685fd4941e50f
cbabit <ignaciotagle26@gmail.com>
2020-10-29 12:56:47 -0300

    Mostrando los resultados de la consulta y primeros pasos para un paginador de resultados

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const formulario = document.querySelector('#formulario');
 const resultado = document.querySelector('#resultado');
+const registrosPorPagina = 40;
+let totalPaginas;
 
 window.onload = () => {
     formulario.addEventListener('submit', validarFormulario);
@@ -12,6 +14,8 @@ function validarFormulario(e) {
         mostrarAlerta('Agrega un término de búsqueda');
         return;
     }
+
+    buscarImagenes(terminoBusqueda);
 }
 
 function mostrarAlerta(mensaje) {
@@ -31,4 +35,50 @@ function mostrarAlerta(mensaje) {
             alerta.remove();
         }, 3000);
     }
+}
+
+function buscarImagenes(termino) {
+    const key = '18821066-963e5fce17f575f7422923fc8';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
+
+    fetch(url)
+        .then((respuesta) => respuesta.json())
+        .then((resultado) => {
+            totalPaginas = calcularPaginas(resultado.totalHits);
+            mostrarImagenes(resultado.hits);
+        });
+}
+
+function calcularPaginas(total) {
+    return parseInt(Math.ceil(total / registrosPorPagina));
+}
+
+function mostrarImagenes(imagenes) {
+    console.log(imagenes);
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Iterar sobre el arreglo de imagenes y construir el HTML
+    imagenes.forEach((imagen) => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white"> 
+                    <img class="w-full" src="${previewURL}">
+
+                    <div class="p-4">
+                        <p class="font-bold"> ${likes} <span class="font-light">Me Gusta</span> </p>
+                        <p class="font-bold"> ${views} <span class="font-light">Views</span> </p>
+                        <a  class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer"
+                        >
+                             Ver Imagen
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
 }
